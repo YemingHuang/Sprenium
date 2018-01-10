4f3f9afd9bee114847754d0bd3c95e2cd52df144
yhuang <yhuang@appliedfinancialtechnology.com>
2018-01-10 14:14:48 +0000

    Initial Commit

---
+++ src/main/java/com/aft/sprenium/SpreniumApplication.java
@@ -0,0 +1,12 @@
+package com.aft.sprenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpreniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpreniumApplication.class, args);
+	}
+}
---
+++ src/test/java/com/aft/sprenium/SpreniumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.aft.sprenium;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpreniumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
